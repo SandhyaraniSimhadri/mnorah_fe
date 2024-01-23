dac2b99b7bd176c3d0b858ec55ad992691456d9f
sandhyasimhadri <sandhyasimhadri999@gmail.com>
2024-01-23 22:06:39 +0530

    new server configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   hmr: false, 
   // apiUrl: 'http://localhost:8000/'
-  apiUrl: 'http://65.0.95.206/'
+  apiUrl: 'http://16.171.168.41/'
   // apiUrl: 'http://localhost:8000/'
 
 };
